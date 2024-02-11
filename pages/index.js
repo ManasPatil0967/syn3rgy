43ce23e3a6addcb9ef5ef8c1dcf5529a15ff143a
ManasPatil0967 <manaspatil0967@gmail.com>
2024-02-11 20:22:57 +0530

    11/02 3

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Component() {
         </div>
       </nav>
         <div className="items-center justify-center ">
-      <section id="title" className="w-full py-12 md:py-24 lg:py-32 xl:py-48 items-center justify-center">
+      <section id="title" className="border-t w-full py-12 md:py-24 lg:py-32 xl:py-48 items-center justify-center">
         <div className="container flex flex-col items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-10">
           <div className="space-y-3">
             <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">SYN3RGY</h1>
@@ -39,7 +39,7 @@ export default function Component() {
           </div>
         </div>
       </section>
-      <section id="about" className="w-full py-12 md:py-24 lg:py-32">
+      <section id="about" className="border-t w-full py-12 md:py-24 lg:py-32">
         <div className="container flex flex-col items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-10">
           <div className="space-y-3">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">About the Hackathon</h2>
@@ -49,7 +49,7 @@ export default function Component() {
           </div>
         </div>
       </section>
-      <section id="timeline" className="w-full py-12 md:py-24 lg:py-32 ">
+      <section id="timeline" className="border-t w-full py-12 md:py-24 lg:py-32 ">
         <div className="container grid items-center gap-4 px-4 text-center md:px-6 lg:gap-10">
           <div className="space-y-3">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">The Timeline</h2>
@@ -113,7 +113,7 @@ export default function Component() {
           </div>
         </div>
       </section>
-      <section id="prizes" className="w-full py-12 md:py-24 lg:py-32">
+      <section id="prizes" className="border-t w-full py-12 md:py-24 lg:py-32">
         <div className="container flex flex-col items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-10">
           <div className="space-y-3">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">Prizes</h2>
@@ -226,7 +226,7 @@ export default function Component() {
           </div>
         </div>
       </section>
-      <section id="sponsors" className="w-full py-12 md:py-24 lg:py-32">
+      <section id="sponsors" className="border-t w-full py-12 md:py-24 lg:py-32">
         <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-10">
           <div className="space-y-3">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">Sponsors</h2>
